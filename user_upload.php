cfcd91524c249c7f5267427af66e948a36c16c77
Daniel Newman <contact@lifeofdan.me>
2021-09-06 22:36:18 +1000

    connect to db

--- user_upload.php
+++ user_upload.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "user upload script \n";
+$dbServer = 'db';
+$dbUser = 'root';
+$dbPassword = 'dbpassword';
+$dbName = 'test';
+
+$db = new mysqli($dbServer, $dbUser, $dbPassword, $dbName);
+
+$usersTable = "CREATE TABLE Users (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(30) NOT NULL,
+	surname VARCHAR(30) NOT NULL,
+	email VARCHAR(50) NOT NULL UNIQUE
+)";
